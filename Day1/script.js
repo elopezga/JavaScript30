05656f3781fe8d5dfceb16235f26af7573f2929c
Edgar Lopez <edgarylop@gmail.com>
2016-12-13 02:18:18 -0800

    Finish Day 1 implementation

--- Day1/script.js
+++ Day1/script.js
@@ -1,8 +1,22 @@
 window.addEventListener('keydown', e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     if( !audio ) return;
+    if( !key ) return;
+
+    key.classList.remove('idleKey');
+    key.classList.add('activeKey');
 
     audio.currentTime = 0;
     audio.play();
 });
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach( key => key.addEventListener('transitionend', e =>{
+    if( e.propertyName !== 'transform') return;
+
+    e.srcElement.classList.remove('activeKey');
+    e.srcElement.classList.add('idleKey');
+}) );
