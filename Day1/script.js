fa225c8f4a46d0bdf74fa4a820be9a2ad3ddf576
Edgar Lopez <edgarylop@gmail.com>
2016-12-13 02:50:25 -0800

    Added click functionality to BeatMachine

--- Day1/script.js
+++ Day1/script.js
@@ -1,6 +1,16 @@
-window.addEventListener('keydown', e => {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function activateKey(e){
+    console.log(e);
+
+    let audio = null;
+    let key = null;
+
+    if( e.type === 'click' ){
+        audio = document.querySelector(`audio[data-key="${e.srcElement.attributes[0].value}"]`);
+        key = document.querySelector(`.key[data-key="${e.srcElement.attributes[0].value}"]`);
+    }else{
+        audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    } 
 
     if( !audio ) return;
     if( !key ) return;
@@ -10,13 +20,18 @@ window.addEventListener('keydown', e => {
 
     audio.currentTime = 0;
     audio.play();
-});
+}
+
+window.addEventListener('keydown', activateKey);
 
 const keys = document.querySelectorAll('.key');
 
-keys.forEach( key => key.addEventListener('transitionend', e =>{
+function setAsIdleKey(e){
     if( e.propertyName !== 'transform') return;
 
     e.srcElement.classList.remove('activeKey');
     e.srcElement.classList.add('idleKey');
-}) );
+}
+
+keys.forEach( key => key.addEventListener('click', activateKey));
+keys.forEach( key => key.addEventListener('transitionend', setAsIdleKey ) );
